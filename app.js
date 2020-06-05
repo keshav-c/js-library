2e3e7b1a85d99b1712e50a627e51279ad8ef5723
keshav-c <keshav1985@gmail.com>
2020-06-05 21:30:05 +0530

    Tinker popup box and Add Book Button

--- app.js
+++ app.js
@@ -14,3 +14,16 @@ function addBookToLibrary() {
 function render() {
 
 }
+
+/* This section adds the popup form */
+
+// get modal
+let addFormBox = document.getElementById('form-box');
+// get modal button
+let addBookBtn = document.getElementById('add-button');
+// get modal close button
+let closeBtn = document.getElementsByClassName('close')[0];
+// make open modal button work
+addBookBtn.onclick = () => { addFormBox.style.display = 'block'; };
+// make modal close button work
+closeBtn.onclick = () => { addFormBox.style.display = 'none'; };
