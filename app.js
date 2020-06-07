b9d00db933efa74ffd3c85722c99bf2a111059a4
keshav-c <keshav1985@gmail.com>
2020-06-07 19:53:24 +0530

    A form to add new books

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+/* This section adds the popup form */
+
+// get modal
+const addFormBox = document.getElementById('form-box');
+// get modal button
+const addBookBtn = document.getElementById('add-button');
+// get modal close button
+const closeBtn = document.getElementsByClassName('close')[0];
+// make open modal button work
+addBookBtn.onclick = () => { addFormBox.style.display = 'block'; };
+// make modal close button work
+closeBtn.onclick = () => { addFormBox.style.display = 'none'; };
+
 const myLibrary = [];
 
 function Book(author, title, pages, read = false) {
@@ -7,23 +20,23 @@ function Book(author, title, pages, read = false) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-
-}
+const addForm = document.forms['add-form'];
+addForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const title = addForm.querySelector('#book-title').value;
+  const author = addForm.querySelector('#book-author').value;
+  const pages = addForm.querySelector('#book-pages').value;
+  const read = Boolean(Number(addForm.querySelector("input[name='read']:checked").value));
+  const book = new Book(author, title, pages, read);
+  myLibrary.push(book);
+  addForm.querySelector('#book-title').value = '';
+  addForm.querySelector('#book-author').value = '';
+  addForm.querySelector('#book-pages').value = '';
+  addForm.querySelector('#book-read').checked = false;
+  addForm.querySelector('#book-unread').checked = true;
+  closeBtn.click();
+});
 
 function render() {
 
 }
-
-/* This section adds the popup form */
-
-// get modal
-let addFormBox = document.getElementById('form-box');
-// get modal button
-let addBookBtn = document.getElementById('add-button');
-// get modal close button
-let closeBtn = document.getElementsByClassName('close')[0];
-// make open modal button work
-addBookBtn.onclick = () => { addFormBox.style.display = 'block'; };
-// make modal close button work
-closeBtn.onclick = () => { addFormBox.style.display = 'none'; };
